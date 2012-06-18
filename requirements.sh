15784688c40cef101a6f76d253e88117dfd3dbb9
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2012-06-18 04:52:08 +0000

    added some missing requirements for ubuntu 12.04

--- requirements.sh
+++ requirements.sh
@@ -10,8 +10,11 @@ $i make
 $i g++
 $i makedepend || $i xutils-dev
 $i flex bison
+$i libreadline-dev libncurses-dev
 $i liblapack-dev libatlas-dev
 $i libatlas-dev
 $i freeglut3-dev
+$i libpng-dev
+$i libbz2-dev
 $i python-numpy python-scipy python-matplotlib python-dev
 
